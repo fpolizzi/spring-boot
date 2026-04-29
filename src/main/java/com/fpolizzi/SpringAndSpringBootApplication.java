a5254059c4129fd7b1eb423d66bc66c26883173b
Franco Polizzi <me@fpolizzi.com>
2026-04-29 19:51:08 +0200

    feat: set-up start code

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.amigoscode;
+package com.fpolizzi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(
                 SpringAndSpringBootApplication.class,
                 args
