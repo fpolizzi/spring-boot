2bbdd086543d2a16dbe25caf189194643c5724ce
Franco Polizzi <me@fpolizzi.com>
2026-04-30 02:06:26 +0200

    feat: add method getPersons

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -3,6 +3,8 @@ package com.fpolizzi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
 
@@ -20,5 +22,10 @@ public class SpringAndSpringBootApplication {
             String name,
             int age,
             Gender gender) {
+    }
+
+    public List<Person> getPersons() {
+
+        return null;
     }
 }
