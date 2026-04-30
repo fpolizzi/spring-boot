dcef7495b6a35bb6dd7dcf30aa4646482f81c7f0
Franco Polizzi <me@fpolizzi.com>
2026-04-30 23:02:50 +0200

    feat: add @RequestParam

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -4,31 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
-
-    static void main(String[] args) {
-        SpringApplication.run(
-                SpringAndSpringBootApplication.class,
-                args
-        );
-    }
-
-    public enum Gender {MALE, FEMALE}
-
-    public record Person(
-            int id,
-            String name,
-            int age,
-            Gender gender) {
-    }
 
     public static List<Person> people = new ArrayList<>();
 
@@ -38,17 +25,50 @@ public class SpringAndSpringBootApplication {
         people.add(new Person(3, "Samba", 29, Gender.MALE));
     }
 
-    @GetMapping
-    public List<Person> getPersons() {
+    static void main(String[] args) {
+        SpringApplication.run(
+                SpringAndSpringBootApplication.class,
+                args
+        );
+    }
 
-        return people;
+    @GetMapping
+    public List<Person> getPersons(
+            @RequestParam(
+                    value = "sort",
+                    required = false,
+                    defaultValue = "ASC"
+            ) SortingOrder sort) {
+        if (sort == SortingOrder.ASC) {
+
+            return people.stream()
+                    .sorted(Comparator.comparing(Person::id))
+                    .collect(Collectors.toList());
+        }
+
+        return people.stream()
+                .sorted(Comparator.comparing(Person::id).reversed())
+                .collect(Collectors.toList());
     }
 
     @GetMapping("{id}")
-    public Optional<Person> getPersonById(@PathVariable Integer id) {
+    public Optional<Person> getPersonById(
+            @PathVariable Integer id
+    ) {
 
         return people.stream()
                 .filter(person -> person.id == id)
                 .findFirst();
+    }
+
+    public enum Gender {MALE, FEMALE}
+
+    public enum SortingOrder {ASC, DESC}
+
+    public record Person(
+            int id,
+            String name,
+            int age,
+            Gender gender) {
     }
 }
