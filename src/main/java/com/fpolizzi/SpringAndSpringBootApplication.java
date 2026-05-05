55fa446dff3e433eab165108fa866fe408c8b3e5
Franco Polizzi <me@fpolizzi.com>
2026-05-05 18:41:32 +0200

    feat: example of Json annotations

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -1,5 +1,7 @@
 package com.fpolizzi;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import org.springframework.boot.SpringApplication;
@@ -106,8 +108,8 @@ public class SpringAndSpringBootApplication {
     public enum SortingOrder {ASC, DESC}
 
     public record Person(Integer id,
-                         String name,
-                         Integer age,
+                         @JsonGetter("foobar") String name,
+                         @JsonIgnore Integer age,
                          Gender gender) {
     }
 }
