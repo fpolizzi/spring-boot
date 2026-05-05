cbe908a852ee0ea104afce5f993fc922dc631ed5
Franco Polizzi <me@fpolizzi.com>
2026-05-05 21:51:49 +0200

    feat: example of serialization and deserialization

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -3,13 +3,29 @@ package com.fpolizzi;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.databind.ObjectMapper;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -32,6 +48,21 @@ public class SpringAndSpringBootApplication {
                 SpringAndSpringBootApplication.class,
                 args
         );
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ObjectMapper objectMapper) {
+
+        String personString = "{\"id\":1, \"name\":\"John Doe\", \"age\":37, \"gender\":\"MALE\"}";
+        Person person = objectMapper.readValue(personString, Person.class);
+
+        // Serialize and Deserialize Object to Json
+        System.out.println(person);
+        System.out.println(objectMapper.writeValueAsString(person));
+
+        return args -> {
+
+        };
     }
 
     @GetMapping
