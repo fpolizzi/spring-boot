c609a9bddb38ab13324da22aaf7d81396d6319cf
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-05-05 21:56:52 +0200

    Json (#6)
    * feat: example of Json annotations
    
    * feat: use class Person instead of record
    
    * feat: example of Json annotations on a class
    
    * feat: example of serialization and deserialization

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -1,17 +1,30 @@
 package com.fpolizzi;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.databind.ObjectMapper;
 
-import java.time.Period;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -35,6 +48,21 @@ public class SpringAndSpringBootApplication {
                 SpringAndSpringBootApplication.class,
                 args
         );
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ObjectMapper objectMapper) {
+
+        String personString = "{\"id\":1, \"name\":\"John Doe\", \"age\":37, \"gender\":\"MALE\"}";
+        Person person = objectMapper.readValue(personString, Person.class);
+
+        // Serialize and Deserialize Object to Json
+        System.out.println(person);
+        System.out.println(objectMapper.writeValueAsString(person));
+
+        return args -> {
+
+        };
     }
 
     @GetMapping
@@ -63,12 +91,12 @@ public class SpringAndSpringBootApplication {
         if (sort == SortingOrder.ASC) {
 
             return people.stream().limit(limit)
-                    .sorted(Comparator.comparing(Person::id))
+                    .sorted(Comparator.comparing(Person::getId))
                     .collect(Collectors.toList());
         }
 
         return people.stream().limit(limit)
-                .sorted(Comparator.comparing(Person::id).reversed())
+                .sorted(Comparator.comparing(Person::getId).reversed())
                 .collect(Collectors.toList());
     }
 
@@ -96,7 +124,7 @@ public class SpringAndSpringBootApplication {
                 new Person(
                         idCounter.incrementAndGet(),
                         person.name,
-                        person.age(),
+                        person.getAge(),
                         person.gender
                 )
         );
@@ -120,8 +148,8 @@ public class SpringAndSpringBootApplication {
                         Person person = new Person(
                                 p.id,
                                 request.name,
-                                p.age(),
-                                p.gender()
+                                p.getAge(),
+                                p.getGender()
                         );
                         people.set(index, person);
                     }
@@ -141,10 +169,64 @@ public class SpringAndSpringBootApplication {
 
     public enum SortingOrder {ASC, DESC}
 
-    public record Person(Integer id,
-                         String name,
-                         Integer age,
-                         Gender gender) {
+//    public record Person(Integer id,
+//                         @JsonGetter("foobar") String name,
+//                         @JsonIgnore Integer age,
+//                         Gender gender) {
+//    }
+
+    public static class Person {
+
+        private final Integer id;
+        private final String name;
+        private final Integer age;
+        private final Gender gender;
+
+        public Person(Integer id, String name, Integer age, Gender gender) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        @JsonIgnore
+        public String getName() {
+            return name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public Gender getGender() {
+            return gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", gender=" + gender +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(id, person.id) && Objects.equals(name, person.name) && Objects.equals(age, person.age) && gender == person.gender;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, age, gender);
+        }
     }
 
     public record PersonUpdate(String name,
