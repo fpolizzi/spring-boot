0274eecfc10e25c3b60a288a313eef1aa311473f
Franco Polizzi <me@fpolizzi.com>
2026-05-27 01:24:28 +0200

    feat: start with a clean starter class

---
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.fpolizzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fpolizzi on 5/27/26
+ */
+@SpringBootApplication
+public class SpringAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SpringAndSpringBootApplication.class,
+                args
+        );
+    }
+}
+
