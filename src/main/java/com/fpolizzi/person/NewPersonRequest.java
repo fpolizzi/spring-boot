f20752cb9a6e75179134f155e211bd972c69e9e9
Franco Polizzi <me@fpolizzi.com>
2026-05-31 00:48:09 +0200

    feat: override Java Bean validation messages

--- src/main/java/com/fpolizzi/person/NewPersonRequest.java
+++ src/main/java/com/fpolizzi/person/NewPersonRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-public record NewPersonRequest(@NotEmpty String name,
-                               @Min(16) Integer age,
-                               @NotNull Gender gender) {
+public record NewPersonRequest(@NotEmpty(message = "Name must not be null or empty") String name,
+                               @Min(value = 16, message = "Age must be equal or greater than 16") Integer age,
+                               @NotNull(message = "Gender must not be null or empty") Gender gender) {
 }
