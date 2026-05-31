b3386cdb0b9ff24a9f354398fc095f6cedee80ea
Franco Polizzi <me@fpolizzi.com>
2026-06-01 00:27:22 +0200

    feat: exercise 2 solution

--- src/main/java/com/fpolizzi/exception/DefaultExceptionHandler.java
+++ src/main/java/com/fpolizzi/exception/DefaultExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.fpolizzi.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -46,6 +48,25 @@ public class DefaultExceptionHandler {
         );
 
         return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handle(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request
+    ) {
+        List<String> errors = e.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                ZonedDateTime.now(),
+                errors
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 
     // any other exception than the specialized above
