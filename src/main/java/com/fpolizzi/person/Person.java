f1acf78b6da2d9bc538c0ea10fdd167e9261fb01
Franco Polizzi <me@fpolizzi.com>
2026-06-06 23:24:19 +0200

    fix: use enumerated to store enum values in the db

--- src/main/java/com/fpolizzi/person/Person.java
+++ src/main/java/com/fpolizzi/person/Person.java
@@ -1,9 +1,6 @@
 package com.fpolizzi.person;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
@@ -14,6 +11,7 @@ public class Person {
     private Integer id;
     private String name;
     private Integer age;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
     private String email;
 
