0e91211aab460b161e9d8d07b05bf789a1aa9659
Franco Polizzi <me@fpolizzi.com>
2026-06-08 00:27:20 +0200

    feat: add existsByEmail to the interface

--- src/main/java/com/fpolizzi/person/PersonRepository.java
+++ src/main/java/com/fpolizzi/person/PersonRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by fpolizzi on 6/6/26
  */
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    boolean existsByEmail(String email);
 }
