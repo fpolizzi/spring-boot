af9d2cef1cbedb358a566e642d78eefe028474d0
Franco Polizzi <me@fpolizzi.com>
2026-06-09 18:17:34 +0200

    feat: add PostController methods

--- src/main/java/com/fpolizzi/post/PostController.java
+++ src/main/java/com/fpolizzi/post/PostController.java
@@ -1,10 +1,10 @@
 package com.fpolizzi.post;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by fpolizzi on 6/9/26
@@ -21,5 +21,27 @@ public class PostController {
     @GetMapping
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
+    }
+
+    @GetMapping("{id}")
+    public Post getPostById(@PathVariable("id") Long id) {
+        return postService.getPostById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPost(@RequestBody Post post) {
+        postService.createPost(post);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePostById(@PathVariable("id") Long id) {
+        postService.deletePost(id);
+    }
+
+    @PutMapping
+    public void updatePost(@RequestBody Post post) {
+        postService.updatePost(post);
     }
 }
