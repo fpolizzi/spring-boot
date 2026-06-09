42ba9b266459e5d9ba2743f806782283cd4374df
Franco Polizzi <me@fpolizzi.com>
2026-06-09 18:17:57 +0200

    feat: add Post record

--- src/main/java/com/fpolizzi/post/Post.java
+++ src/main/java/com/fpolizzi/post/Post.java
@@ -4,7 +4,7 @@ package com.fpolizzi.post;
  * Created by fpolizzi on 6/9/26
  */
 public record Post(
-        Integer id,
+        Long id,
         Integer userId,
         String title,
         String body
