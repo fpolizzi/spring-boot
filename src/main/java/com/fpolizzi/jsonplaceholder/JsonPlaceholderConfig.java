506494b07401428860c19b959fd5e252e1ef6e4e
Franco Polizzi <me@fpolizzi.com>
2026-06-10 17:23:59 +0200

    feat: customize statsus errors

--- src/main/java/com/fpolizzi/jsonplaceholder/JsonPlaceholderConfig.java
+++ src/main/java/com/fpolizzi/jsonplaceholder/JsonPlaceholderConfig.java
@@ -3,8 +3,12 @@ package com.fpolizzi.jsonplaceholder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 /**
@@ -29,15 +33,16 @@ public class JsonPlaceholderConfig {
     @Primary
     @Bean("web-client")
     JsonPlaceholderService jsonPlaceholderWebClientService() {
-        RestClient restClient = RestClient.create(
-                "https://jsonplaceholder.typicode.com"
-        );
+        WebClient restClient = WebClient.builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .defaultStatusHandler(HttpStatusCode::isError,
+                        clientResponse -> {
+                            throw new ResponseStatusException(clientResponse.statusCode());
+                        }).build();
 
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory
-                .builderFor(RestClientAdapter.create(restClient))
-                .build();
-
-        return factory.createClient(JsonPlaceholderService.class);
+        return HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(restClient))
+                .build()
+                .createClient(JsonPlaceholderService.class);
     }
-
 }
