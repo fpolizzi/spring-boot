49f619f9da13145ec77afaffcc86543bab507cc4
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-06-11 01:13:33 +0200

    task execution and scheduling (#15)
    * feat: basic example of task scheduling
    
    * feat: example of task scheduling via cron notation
    
    * feat: two task executions
    
    * feat: configure tasks that can run parallel
    
    * feat: run tasks asynchronously

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -2,11 +2,19 @@ package com.fpolizzi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by fpolizzi on 5/27/26
  */
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class SpringAndSpringBootApplication {
 
     static void main(String[] args) {
@@ -14,6 +22,33 @@ public class SpringAndSpringBootApplication {
                 SpringAndSpringBootApplication.class,
                 args
         );
+    }
+
+    @Scheduled(
+            fixedRate = 5,
+            timeUnit = TimeUnit.SECONDS
+    )
+    @Async
+    public void sendEmails() throws InterruptedException {
+
+        System.out.println("start sending emails");
+
+        Thread.sleep(2000);
+
+        System.out.println("end sending emails");
+    }
+
+    @Scheduled(
+            cron = "*/5 * * * * *"
+    )
+    @Async
+    public void generateSalesReport() throws InterruptedException {
+
+        System.out.println("start sales report");
+
+        Thread.sleep(5000);
+
+        System.out.println("end sales report");
     }
 }
 
