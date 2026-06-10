c24c82baaad1fe7b13c7d6e45bc6da3b493a2b6c
Franco Polizzi <me@fpolizzi.com>
2026-06-11 01:09:35 +0200

    feat: run tasks asynchronously

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -2,6 +2,8 @@ package com.fpolizzi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -12,6 +14,7 @@ import java.util.concurrent.TimeUnit;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class SpringAndSpringBootApplication {
 
     static void main(String[] args) {
@@ -25,6 +28,7 @@ public class SpringAndSpringBootApplication {
             fixedRate = 5,
             timeUnit = TimeUnit.SECONDS
     )
+    @Async
     public void sendEmails() throws InterruptedException {
 
         System.out.println("start sending emails");
@@ -37,6 +41,7 @@ public class SpringAndSpringBootApplication {
     @Scheduled(
             cron = "*/5 * * * * *"
     )
+    @Async
     public void generateSalesReport() throws InterruptedException {
 
         System.out.println("start sales report");
