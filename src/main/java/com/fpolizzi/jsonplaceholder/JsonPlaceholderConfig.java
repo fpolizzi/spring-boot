00d97d74d4be84d4f751e6aaba2a82f16e873356
Franco Polizzi <me@fpolizzi.com>
2026-06-10 17:03:22 +0200

    feat: use web-client instead of rest-client

--- src/main/java/com/fpolizzi/jsonplaceholder/JsonPlaceholderConfig.java
+++ src/main/java/com/fpolizzi/jsonplaceholder/JsonPlaceholderConfig.java
@@ -2,6 +2,7 @@ package com.fpolizzi.jsonplaceholder;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -12,8 +13,8 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class JsonPlaceholderConfig {
 
-    @Bean
-    JsonPlaceholderService jsonPlaceholderService() {
+    @Bean("rest-client")
+    JsonPlaceholderService jsonPlaceholderRestClientService() {
         RestClient restClient = RestClient.create(
                 "https://jsonplaceholder.typicode.com"
         );
@@ -24,4 +25,19 @@ public class JsonPlaceholderConfig {
 
         return factory.createClient(JsonPlaceholderService.class);
     }
+
+    @Primary
+    @Bean("web-client")
+    JsonPlaceholderService jsonPlaceholderWebClientService() {
+        RestClient restClient = RestClient.create(
+                "https://jsonplaceholder.typicode.com"
+        );
+
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+                .builderFor(RestClientAdapter.create(restClient))
+                .build();
+
+        return factory.createClient(JsonPlaceholderService.class);
+    }
+
 }
