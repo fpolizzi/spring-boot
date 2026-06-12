c181f751e22cbb09664a377853ce6797e7067297
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-06-12 18:55:21 +0200

    application configuration (#16)
    * feat: define a user defined property/value
    
    * feat: output value of user defined property
    
    * feat: use Environment to output property
    
    * feat: use a separate config class
    
    * feat: env variables and commandline aeguments
    
    * feat: work with different profiles

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -1,13 +1,15 @@
 package com.fpolizzi;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
 
 /**
  * Created by fpolizzi on 5/27/26
@@ -17,38 +19,33 @@ import java.util.concurrent.TimeUnit;
 @EnableAsync
 public class SpringAndSpringBootApplication {
 
+    @Value("${user.my-name}")
+    private String userName;
+
+    private Environment environment;
+
     static void main(String[] args) {
+
+        // output args
+        System.out.println("args: " + Arrays.toString(args));
+
         SpringApplication.run(
                 SpringAndSpringBootApplication.class,
                 args
         );
     }
 
-    @Scheduled(
-            fixedRate = 5,
-            timeUnit = TimeUnit.SECONDS
-    )
-    @Async
-    public void sendEmails() throws InterruptedException {
+    @Bean
+    CommandLineRunner commandLineRunner(
+            Environment environment,
+            StripeConfig stripeConfig
+    ) {
 
-        System.out.println("start sending emails");
+        System.out.println(userName);
+        System.out.println(environment.getProperty("user.my-name"));
+        System.out.println(stripeConfig);
 
-        Thread.sleep(2000);
-
-        System.out.println("end sending emails");
-    }
-
-    @Scheduled(
-            cron = "*/5 * * * * *"
-    )
-    @Async
-    public void generateSalesReport() throws InterruptedException {
-
-        System.out.println("start sales report");
-
-        Thread.sleep(5000);
-
-        System.out.println("end sales report");
+        return args -> {};
     }
 }
 
---
+++ src/main/java/com/fpolizzi/StripeConfig.java
@@ -0,0 +1,39 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 6/12/26
+ */
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.stripe")
+public class StripeConfig {
+
+    private String url;
+    private String apiKey;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public String toString() {
+        return "StripeConfig{" +
+                "url='" + url + '\'' +
+                ", apiKey='" + apiKey + '\'' +
+                '}';
+    }
+}
