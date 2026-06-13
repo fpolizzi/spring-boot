eb9e77f2e2c34e70515fb0ed2e9bbff61f38275e
Franco Polizzi <me@fpolizzi.com>
2026-06-13 04:43:07 +0200

    feat: configure logging

--- src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -1,15 +1,9 @@
 package com.fpolizzi;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Arrays;
 
 /**
  * Created by fpolizzi on 5/27/26
@@ -19,33 +13,13 @@ import java.util.Arrays;
 @EnableAsync
 public class SpringAndSpringBootApplication {
 
-    @Value("${user.my-name}")
-    private String userName;
-
-    private Environment environment;
-
     static void main(String[] args) {
-
-        // output args
-        System.out.println("args: " + Arrays.toString(args));
-
         SpringApplication.run(
                 SpringAndSpringBootApplication.class,
                 args
         );
-    }
 
-    @Bean
-    CommandLineRunner commandLineRunner(
-            Environment environment,
-            StripeConfig stripeConfig
-    ) {
-
-        System.out.println(userName);
-        System.out.println(environment.getProperty("user.my-name"));
-        System.out.println(stripeConfig);
-
-        return args -> {};
+        System.out.println("Hello");
     }
 }
 
