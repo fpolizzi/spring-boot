90e56fc0695bd0736414c0971cb38d5a37038a9f
Franco Polizzi <me@fpolizzi.com>
2026-06-16 00:42:12 +0200

    feat: add a custom user with an encoded password

--- src/main/java/com/fpolizzi/security/SecurityConfig.java
+++ src/main/java/com/fpolizzi/security/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -26,5 +32,24 @@ public class SecurityConfig {
                 })
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(
+            PasswordEncoder passwordEncoder
+    ) {
+
+        UserDetails userDetails = User.builder()
+                .username("fpolizzi")
+                .password(passwordEncoder.encode("test55"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
